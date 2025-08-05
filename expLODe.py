6226c43c5c7bd87d42062721a2c39514a650ddb2
RainMa22 <d2rain@126.com>
2025-08-04 20:25:35 -0700

    modulefy expLODe.py

--- expLODe.py
+++ expLODe.py
@@ -1,52 +1,68 @@
 import bpy
+import sys
 import shutil
 import os
 import json
 import subprocess
 
 # loads attempts to find blender and use it for later scripts
-config : dict = {}
+__config : dict = {}
 
-if(not os.path.exists("config.json")):
-    shutil.copyfile("config.default.json","config.json")
+def load_config():
+    global __config
+    if(not os.path.exists("config.json")):
+        shutil.copyfile("config.default.json","config.json")
 
-with open("config.json", "r") as file:
-    config = json.loads(file.read())
+    with open("config.json", "r") as file:
+        __config = json.loads(file.read())
 
-blender_command = config.get("expLODe.blenderCmd")
-if(blender_command is None or blender_command == ""):
-    print("Blender path not defined! Attempting to find automatically...")
-    blenderPath = shutil.which("blender")
-    if(blenderPath is None):
-        print("Blender is not on PATH!")
-    else: 
-        blender_command = blenderPath
+    blender_command = __config.get("expLODe.blenderCmd")
+    if(blender_command is None or blender_command == ""):
+        print("Blender path not defined! Attempting to find automatically...")
+        blenderPath = shutil.which("blender")
+        if(blenderPath is None):
+            print("Blender is not on PATH!")
+        else: 
+            blender_command = blenderPath
 
-config["expLODe.blenderCmd"] = blender_command if blender_command is not None else ""
+    __config["expLODe.blenderCmd"] = blender_command if blender_command is not None else ""
 
-with open("config.json", "w") as file:
-    config = file.write(json.dumps(config))
+    with open("config.json", "w") as file:
+        file.write(json.dumps(__config))
 
-if blender_command is None or blender_command == "":
-    print("Unable to Find Blender!")
-    print("Please configure `expLODe.blenderCmd` in config.json!")
-    print("Exiting...")
-    exit()
+def get_config():
+    return __config.copy()
 
-print("Blender found, checking version")
+def check_version():
+    blender_command = get_config().get("expLODe.blenderCmd")
+    if blender_command is None or blender_command == "":
+        print("Unable to Find Blender!")
+        print("Please configure `expLODe.blenderCmd` in config.json!")
+        print("Exiting...")
+        exit()
 
-v_string=""
-v_ver=""
-v_major=-1
-v_minor=-1
-v_patch=-1
-with subprocess.Popen((blender_command + " --version").split(" "), stdout=subprocess.PIPE).stdout as proc_out:
-    v_string = proc_out.readline().decode()
-    v_ver = v_string.split(" ")[1]
-    v_major,v_minor,v_patch = (int(x) for x in v_ver.split("."))
+    print("Blender found, checking version")
 
-print(v_major,v_minor,v_patch, sep=".")
-if(v_major != 4 or v_minor < 2):
-    print(f"invalid version {v_ver}! Blender >= 4.2, < 5.0.0 expected!")
-    
-print("blender version satisfied")
+    v_string=""
+    v_ver=""
+    v_major=-1
+    v_minor=-1
+    v_patch=-1
+    with subprocess.Popen((blender_command + " --version").split(" "), stdout=subprocess.PIPE).stdout as proc_out:
+        v_string = proc_out.readline().decode()
+        v_ver = v_string.split(" ")[1]
+        v_major,v_minor,v_patch = (int(x) for x in v_ver.split("."))
+
+    print(v_major,v_minor,v_patch, sep=".")
+    if(v_major != 4 or v_minor < 2):
+        print(f"invalid version {v_ver}! Blender >= 4.2, < 5.0.0 expected!")
+        
+    print("blender version satisfied")
+
+def main():
+    load_config()
+    check_version()
+
+if __name__ == "__main__":
+    main()
+
